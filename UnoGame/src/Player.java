5736d097147913803e309271ba59465730edd625
jpsilverio1 <jpsilverio1@gmail.com>
2016-10-11 13:52:10 -0400

    Adding validation method for player

--- UnoGame/src/Player.java
+++ UnoGame/src/Player.java
@@ -4,4 +4,5 @@ public interface Player {
 	public List<Card> getHand();
 	public int getOrder();
 	public String getName();
+	private boolean validateMove(Card currentCardOnTable, Card cardToBePlayed);
 }
