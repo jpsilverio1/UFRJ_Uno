9d778ca623925e3f5923bd1e06b0202566f6c85c
jpsilverio1 <jpsilverio1@gmail.com>
2016-12-13 20:41:43 -0500

    Adding Player class so that I can use it on tests

---
+++ UnoGame/src/main/Player.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.List;
+
+public class Player implements IPlayer {
+
+	@Override
+	public List<ICard> getHand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean validateMove(ICard currentCardOnTable, ICard cardToBePlayed) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
