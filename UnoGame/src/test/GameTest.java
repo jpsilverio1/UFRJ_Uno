5219c160ddf4922604371f3c8f8a4952829bf2c0
HectorNM <hectornmelo@gmail.com>
2016-12-15 16:33:10 -0200

    New tests added

--- UnoGame/src/test/GameTest.java
+++ UnoGame/src/test/GameTest.java
@@ -34,16 +34,16 @@ public class GameTest {
 		mockPlayer(player3, 3, "Jose");
 		mockPlayer(player4, 4, "Carolina");
 		mockPlayer(player5, 5, "Beatriz");
+		when(initialCard.getColor()).thenReturn(Color.RED);
 		List<IPlayer> players = Arrays.asList(player1, player2, player3, player4, player5);
 		fixture = new Game(players, initialCard);
 	}
-	/* ID 0 : AÃ§Ã£o No Action
-		ID 1 : AÃ§Ã£o Skip
-		ID 2 : AÃ§Ã£o Reverse
-		ID 3 : AÃ§Ã£o Draw 2
-		ID 4 : AÃ§Ã£o Wild
-		ID 5 : AÃ§Ã£o Wild Draw 4
-	*/
+	/* ID 0 : Ação No Action
+ID 1 : Ação Skip
+ID 2 : Ação Reverse
+ID 3 : Ação Draw 2
+ID 4 : Ação Wild
+ID 5 : Ação Wild Draw 4*/
 	@Test 
 	public void testGameDirection() {
 		Card cardPlayed = mockGameCard(Color.BLUE, 0);
@@ -76,11 +76,20 @@ public class GameTest {
 	}
 	@Test
 	public void testDrawCards() {
-		
+		Card cardPlayed = mockGameCard(Color.BLUE, 3);
+		Card cardPlayed2 = mockGameCard(Color.MULTICOLOR, 5);
+		Action action = Action.MOVE;
+		MoveOutput output = fixture.makeAMove(player2, cardPlayed, action, null);
+		assertOutputValues(output, player3, player3.getName(), Action.DRAW, 2, cardPlayed, cardPlayed.getColor());
+		MoveOutput output2 = fixture.makeAMove(player4, cardPlayed2, action, Color.YELLOW);
+		assertOutputValues(output2, player5, player5.getName(), Action.DRAW, 4, cardPlayed2, Color.YELLOW);
 	}
 	@Test
 	public void testSkipCards() {
-		
+		Card cardPlayed = mockGameCard(Color.GREEN, 1);
+		Action action = Action.MOVE;
+		MoveOutput output = fixture.makeAMove(player5, cardPlayed, action, null);
+		assertOutputValues(output, player1, player1.getName(), Action.SKIP, 0, cardPlayed, cardPlayed.getColor());
 	}
 	@Test
 	public void testDrawWildCard() {
@@ -93,20 +102,33 @@ public class GameTest {
 	
 	@Test
 	public void testNextPlayer() {
-		Card cardPlayed = mockGameCard(Color.BLUE, 1);
+		Card cardPlayed = mockGameCard(Color.BLUE, 0);
 		Action action = Action.MOVE;
-		MoveOutput output = fixture.makeAMove(player2, cardPlayed, action, null);		
+		MoveOutput output = fixture.makeAMove(player2, cardPlayed, action, null);
+		assertOutputValues(output, player3, player3.getName(),Action.MOVE, 0, cardPlayed, Color.BLUE);
 	}
+	
+	@Test
+	public void testNextPlayerEdge() {
+		Card cardPlayed = mockGameCard(Color.BLUE, 0);
+		Action action = Action.MOVE;
+		MoveOutput output = fixture.makeAMove(player5, cardPlayed, action, null);
+		assertOutputValues(output, player1, player1.getName(),Action.MOVE, 0, cardPlayed, Color.BLUE);
+			
+	}
+	
+	@Test
+	public void testPlayerDrew() {
+		Action action = Action.DRAW;
+		MoveOutput output = fixture.makeAMove(player1, null, action, null);
+		assertOutputValues(output, player2, player2.getName(),Action.MOVE, 0, initialCard, initialCard.getColor());
+
+	}
+	
 	@Test
 	public void another() {
 				
 	}
-
-	@Test
-    public void testFinishGame(){
-
-    }
-
 	private void assertOutputValues(MoveOutput output,
 			IPlayer expectedNextPlayer, 
 			String expectedNextPlayerName, 
@@ -121,14 +143,12 @@ public class GameTest {
 		assertEquals(expectedCardOnTopOfFile, output.getCardOnTopOnDiscardPile());
 		assertEquals(expectedNextColorToPlay, output.getNextColorToPlay());
 	}
-
 	private Card mockGameCard(Color color, int id) {
 		Card card = mock(Card.class);
 		when(card.getID()).thenReturn(id);
 		when(card.getColor()).thenReturn(color);
 		return card;
 	}
-
 	private void mockPlayer(IPlayer player, int order, String name) {
 		when(player.getOrder()).thenReturn(order);
 		when(player.getName()).thenReturn(name);
